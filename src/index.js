d2a667a626fa061dbf5608dcefddbca636d2b1c8
Jannis Pohlmann <jannis@thegraph.com>
2020-11-18 00:32:50 +0100

    Remove bold keywords, they cause broken pipes

--- src/index.js
+++ src/index.js
@@ -3,41 +3,48 @@
 const { start } = require('pino-tiny')
 const chalk = require('chalk')
 
+function levelColor(level) {
+  return level >= 50
+    ? chalk.red
+    : level >= 40
+    ? chalk.yellow
+    : level >= 30
+    ? chalk.green
+    : level >= 20
+    ? chalk.white
+    : chalk.gray
+}
+
 function filter(data) {
-  let msg = `${data.msg}`
+  let msg = data.msg
 
   if (data.deployment) {
-    msg += `, ${chalk.bold(deployment)}: ${data.deployment.ipfsHash || data.deployment}`
+    msg += `, deployment: ${data.deployment.ipfsHash || data.deployment}`
   }
 
   if (data.indexer) {
-    msg += `, ${chalk.bold(indexer)}: ${data.indexer}`
+    msg += `, indexer: ${data.indexer}`
   }
 
   if (data.allocation || data.allocationId) {
-    msg += `, ${chalk.bold(allocation)}: ${data.allocation || data.allocationId}`
+    msg += `, allocation: ${data.allocation || data.allocationId}`
   }
 
   if (data.channel || data.channelId) {
-    msg += `, ${chalk.bold(channel)}: ${data.channel || data.channelId}`
+    msg += `, channel: ${data.channel || data.channelId}`
   }
 
   if (data.err) {
-    msg += `, ${chalk.bold(err)}: ${data.err.message.replace(/\n/, '')}`
+    msg += `, err: ${
+      data.err.message
+        ? data.err.message.replace(/\n/, '')
+        : JSON.stringify(data.err).replace(/\n/, '')
+    }`
   }
 
   return {
     ...data,
-    msg:
-      data.level >= 50
-        ? chalk.red(msg)
-        : data.level >= 40
-        ? chalk.yellow(msg)
-        : data.level >= 30
-        ? chalk.green(msg)
-        : data.level >= 20
-        ? chalk.white(msg)
-        : chalk.gray(msg),
+    msg: levelColor(data.level)(msg),
   }
 }
 
